2a84af1eaea463a39bac5ebb0f760cb7066ea160
j0rem0rV2 <phanthonj0rem0rV2@proton.me>
2024-10-29 23:52:04 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+
+/*
+ * Singleton "Lazy Holder"
+ * 
+ * @author 
+ */
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    private SingletonLazyHolder(){
+        super();
+    }
+    
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
+
